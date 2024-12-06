bc01084c6d3ac27f3188cd66f59d94093e48949b
Grenade Inspector <grenade_inspector@tutamail.com>
2024-12-06 09:56:37 -0500

    Fixed popup detection

--- src/screen.py
+++ src/screen.py
@@ -20,8 +20,8 @@ SCREEN_HEIGHT = win32.GetSystemMetrics(1)
 SCALE_WIDTH = SCREEN_WIDTH/1920 # screen width/1920
 SCALE_HEIGHT = SCREEN_HEIGHT/1080 # screen height/1080
 
-coords = {"play_again": (375, 505, 174, 250), "operators": (208, 318, 52, 90), "locker": (374, 450, 52, 90), "queueing": (808, 1026, 38, 68), "find_another_match": (1261, 1465, 1002, 1033), "popup_message": (699, 965, 927, 951), "reconnect": (837, 1080, 38, 68), "reconnect_queue": (385, 530, 185, 251)}
-keywords = {"play_again": ["play again"], "operators": ["operators"], "locker": ["locker"], "queueing": ["crossplay"], "find_another_match": ["find another"], "popup_message": ["ok", "cancel", "reconnect"], "reconnect": ["reconnect"], "reconnect_queue": ["reconnect"]}
+coords = {"play_again": (375, 505, 174, 250), "operators": (208, 318, 52, 90), "locker": (374, 450, 52, 90), "queueing": (808, 1026, 38, 68), "find_another_match": (1261, 1465, 1002, 1033), "ok_popup": (692, 727, 920, 950), "other_popup_messages": (699, 965, 927, 951), "reconnect": (837, 1080, 38, 68), "reconnect_queue": (385, 530, 185, 251)}
+keywords = {"play_again": ["play again"], "operators": ["operators"], "locker": ["locker"], "queueing": ["crossplay"], "find_another_match": ["find another"], "ok_popup": ["ok"], "other_popup_messages": ["cancel", "reconnect"], "reconnect": ["reconnect"], "reconnect_queue": ["reconnect"]}
 
 def get_res_scale_x(x):
     return int(SCALE_WIDTH * x)
@@ -43,7 +43,7 @@ def read_screenshot(coords_type=None, keyword=None):
             return False
 
 def scrape(mnk):
-    state = {"in_lobby": False, "queueing": False, "in_game": True, "reconnect": False, "popup": False, "end_of_game": False}
+    state = {"in_lobby": False, "queueing": False, "in_game": True, "reconnect": False, "ok_popup": False, "other_popups": False, "end_of_game": False}
 
     state["in_lobby"] = read_screenshot("play_again", keywords["play_again"][0])
     if not state["in_lobby"]:
@@ -52,10 +52,11 @@ def scrape(mnk):
 
     state["queueing"] = read_screenshot("queueing", keywords["queueing"][0])
     state["end_of_game"] = read_screenshot("find_another_match", keywords["find_another_match"][0])
+    state["ok_popup"] = read_screenshot("ok_popup", keywords["ok_popup"][0])
 
-    for x in range(len(keywords["popup_message"])):
-        state["popup_message"] = read_screenshot("popup_message", keywords["popup_message"][x])
-        if state["popup_message"]:
+    for x in range(len(keywords["other_popup_messages"])):
+        state["other_popups"] = read_screenshot("other_popup_messages", keywords["other_popup_messages"][x])
+        if state["other_popups"]:
             state["in_lobby"] = False
             state["queueing"] = False
             state["in_game"] = False
